01853745b61340b11a57fe0d62d9c41856c0755d
ydhwa <ydhwa_18@naver.com>
2019-07-03 15:01:31 +0900

    javascript 기초

---
+++ wp_js1/WebContent/rect.js
@@ -0,0 +1,23 @@
+/**
+ * Rect 생성자 함수 
+ */
+
+var Rect = function(x1, y1, x2, y2, bg) {
+	// 반드시 있어야 하는 것이므로 this를 사용한다.
+	this.x1 = x1;
+	this.y1 = y1;
+	this.x2 = x2;
+	this.y2 = y2;
+	this.bg = bg;
+}
+
+Rect.prototype.show = function() {
+	document.write(
+		'<div style="position: absolute; ' + 
+			'left: ' + this.x1 + 'px; ' + 
+			'top: ' + this.y1 + 'px; ' +
+			'width: ' + (this.x2 - this.x1) + 'px; ' +
+			'height: ' + (this.y2 - this.y1) + 'px; ' + 
+			'background-color: ' + this.bg + ';">' + 
+		'</div>');
+}
